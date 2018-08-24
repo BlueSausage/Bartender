58026f7910bd26d7414a7186b6454cffcbbaf1c6
BlueSausage <BlueSausage@users.noreply.github.com>
2018-08-25 00:42:26 +0200

    Implemented filter for drinks
    Added filterDrinks method in bartender.py and a new file menu.py.

--- bartender.py
+++ bartender.py
@@ -1,9 +1,10 @@
 import time
 import sys
 import json
+import itertools
 
-
-from drinks import  drink_list, drink_options
+from drinks import drink_list, drink_options
+from menu import MenuItem
 
 FLOW_RATE = 60./100.0
 
@@ -12,10 +13,9 @@ class Bartender:
     def __init__(self):
         self.running = False
 
+
         # load the pump configuration from file
         self.pump_configuration = Bartender.readPumpConfiguration()
-        for pump in self.pump_configuration.keys():
-            GPIO.setup(self.pump_configuration[pump]["pin"], GPIO.OUT, initial=GPIO.HIGH)
 
     @staticmethod
     def readPumpConfiguration():
@@ -23,16 +23,52 @@ class Bartender:
 
     @staticmethod
     def writePumpConfiguration(configuration):
-        with open("pump_config.json", "w") as jsonFile:
+        with open('pump_config.json', 'w') as jsonFile:
             json.dump(configuration, jsonFile)
+
 
     def showPumpConfiguration(self):
         for p in sorted(self.pump_configuration.keys()):
-            print(self.pump_configuration[p]["name"], self.pump_configuration[p]["value"])
+            print(self.pump_configuration[p]['name'], self.pump_configuration[p]['value'])
 
-    def changePumpConfiguration(self, name, value):
+    def changePumpConfiguration(self, name, newValue):
         for p in sorted(self.pump_configuration.keys()):
-            if self.pump_configuration[p]["name"] == name:
-                self.pump_configuration[p]["value"] = value
+            if self.pump_configuration[p]['name'] == name:
+                self.pump_configuration[p]['value'] = newValue
                 self.writePumpConfiguration(self.pump_configuration)
                 break
+
+    def filterDrinks(self):
+        """
+                Removes any drinks that can't be handled by the pump configuration
+        """
+        drinks = []
+        for d in drink_list:
+            drinks.append(MenuItem(d['name'], d['ingredients'].keys(), False))
+
+        n = 0
+        k = 0
+        ingredients = 0
+        while n < len(drinks):
+            while k < len(list(drinks.__getitem__(n).attributes)):
+                for p in sorted(self.pump_configuration):
+                    if self.pump_configuration[p]['value'] is not None:
+                        if list(drinks.__getitem__(n).attributes).__getitem__(k) == self.pump_configuration[p]['value']:
+                            ingredients += 1
+                k += 1
+            if len(list(drinks.__getitem__(n).attributes)) == ingredients:
+                drinks.__getitem__(n).visible = True
+            ingredients = 0
+            k = 0
+            n += 1
+
+        j = 0
+        while j < len(drinks):
+            if drinks.__getitem__(j).visible is True:
+                print(drinks.__getitem__(j).name)
+            j += 1
+
+
+if __name__ == '__main__':
+    bartender = Bartender()
+    bartender.filterDrinks()
---
+++ menu.py
@@ -0,0 +1,5 @@
+class MenuItem(object):
+    def __init__(self, name, attributes=None, visible=False):
+        self.name = name
+        self.attributes = attributes
+        self.visible = visible
