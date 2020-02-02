91fa1bb6e6d72f34664fe986e2b561115e5389cc
BlueSausage <36741399+BlueSausage@users.noreply.github.com>
2020-02-02 13:50:57 +0100

    change gui package

--- menu.py
+++ menu.py
@@ -1,4 +1,41 @@
-import math
+import sys
+import random
+from PySide2 import QtCore, QtWidgets, QtGui
+
+
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+
+        self.button = QtWidgets.QPushButton("Click me!")
+        self.text = QtWidgets.QLabel("Hello World")
+        self.text.setAlignment(QtCore.Qt.AlignCenter)
+
+        self.layout = QtWidgets.QVBoxLayout()
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+        self.setLayout(self.layout)
+
+        self.button.clicked.connect(self.magic)
+
+
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec_())
+
+
+'''import math
 try:
     from Tkinter import *
 except ImportError:
@@ -40,3 +77,4 @@ class Menu():
 root = Tk()
 menu = Menu(root)
 root.mainloop()
+'''
